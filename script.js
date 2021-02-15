bf611d21ec5ce9f0150db20c32114bf97daa16ad
hrt99 <63954960+hrt99@users.noreply.github.com>
2021-02-15 18:03:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function setPixelColour(pixel)
+{pixel.style.backgroundColor = 'black';
+}
+var penColour = 'black';
+
+function setPenColour(pen)
+{penColour = pen;
+}
+
+function setPixelColour(pixel)
+{pixel.style.backgroundColor = penColour;
+}
